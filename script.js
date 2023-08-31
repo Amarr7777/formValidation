7b03d0632f915debf02316c1d5e88df61ee1d328
Amar G Nath <84448118+Amarr7777@users.noreply.github.com>
2023-08-31 19:56:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,10 +8,14 @@ function validate() {
     var reName = /^[A-Za-z ]{3,}$/;
     var reEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     var rePass = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+    var reDate = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/
 
     var year = dob.value.slice(0,4);
     var dobNew = parseInt(year);
-    var eighteenYearsAgo = 2023-dobNew 
+    var today = new Date().getFullYear()
+    var thisYear = parseInt(today)
+    console.log(dob.value);
+    var eighteenYearsAgo = thisYear-dobNew 
 
     if (name.value.trim() === "" || !reName.test(name.value)) {
         window.alert("Enter your name");
@@ -28,12 +32,14 @@ function validate() {
     } else if (cPassword.value !== password.value) {
         window.alert("Passwords are not matching");
         console.log("Passwords are not matching");
-    } else if(eighteenYearsAgo < 18 ){
+    } else if(dob.value === "" || !reDate.test(dob.value)){
+        window.alert("Enter your date of birth");
+    }  else if(eighteenYearsAgo < 18 ){
         window.alert("You should be atleast 18 years old");
         console.log("You should be atleast 18 years old");
-    }
-    else {
+    } else {
         window.alert("Form submitted successfully");
         console.log("Form submitted successfully");
+        window.location.reload();
     }
 }
